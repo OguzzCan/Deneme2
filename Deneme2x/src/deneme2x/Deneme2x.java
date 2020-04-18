fd0cdc5d145151b8420113b991213178b477f144
Oğuz <oguzcan.varoll@gmail.com>
2020-04-18 07:21:23 +0200

    Proje eklendi.

---
+++ Deneme2x/src/deneme2x/Deneme2x.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme2x;
+
+/**
+ *
+ * @author Oğuz Can
+ */
+public class Deneme2x {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int y = 5;
+    }
+    
+}
